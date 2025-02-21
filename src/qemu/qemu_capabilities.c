f7424339d2fbb0434adb3f6535accd0af8d1598d
Matthew R. Ochs <mochs@nvidia.com>
2025-02-21 07:30:32 -0800

    qemu: Add capability for PCI high memory MMIO size
    Add QEMU capability for PCI high memory MMIO size configuration:
    - Add QEMU_CAPS_MACHINE_VIRT_HIGHMEM_MMIO_SIZE capability
    - Add capability to virt machine properties
    - Add highmem-mmio-size virt machine property to aarch64 qemu
          10.0.0 capabilities
    
    This allows detecting support for the highmem-mmio-size virt machine
    property in QEMU.
    
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -732,6 +732,7 @@ VIR_ENUM_IMPL(virQEMUCaps,
 
               /* 475 */
               "virtio-scsi.iothread-mapping", /* QEMU_CAPS_VIRTIO_SCSI_IOTHREAD_MAPPING */
+              "machine.virt.highmem-mmio-size", /* QEMU_CAPS_MACHINE_VIRT_HIGHMEM_MMIO_SIZE */
     );
 
 
@@ -1767,6 +1768,7 @@ static struct virQEMUCapsStringFlags virQEMUCapsMachinePropsVirt[] = {
     { "iommu", QEMU_CAPS_MACHINE_VIRT_IOMMU },
     { "ras", QEMU_CAPS_MACHINE_VIRT_RAS },
     { "aia", QEMU_CAPS_MACHINE_VIRT_AIA },
+    { "highmem-mmio-size", QEMU_CAPS_MACHINE_VIRT_HIGHMEM_MMIO_SIZE },
 };
 
 static struct virQEMUCapsStringFlags virQEMUCapsMachinePropsGeneric[] = {
